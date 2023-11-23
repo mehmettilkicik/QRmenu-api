d5c91ec3f734da3653ac77351934f9bd1cc9e3a8
mehmettilkicik <pbmemedo@gmail.com>
2023-11-23 03:58:53 +0300

    created

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("api", welcome)
+	log.Fatal(app.Listen(":3000"))
+
+}
+
+func welcome(c *fiber.Ctx) error {
+	return c.Status(200).JSON("welcome")
+}
