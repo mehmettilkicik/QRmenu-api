a43abf7e117b82f681c8e6515cb0052e3acec9ac
mehmettilkicik <pbmemedo@gmail.com>
2023-11-23 15:50:10 +0300

    item table updated

--- models/item.go
+++ models/item.go
@@ -5,7 +5,7 @@ import "time"
 type Item struct {
 	ID            uint `json:"id" gorm:"primaryKey"`
 	CreatedAt     time.Time
-	Name          string `json:"name"`
-	CategoryRefer uint   `json:"category_id"`
-	Category      string `gorm:"foreignKey:CategoryRefer"`
+	Name          string   `json:"name"`
+	CategoryRefer uint     `json:"category_id"`
+	Category      Category `gorm:"foreignKey:CategoryRefer"`
 }
