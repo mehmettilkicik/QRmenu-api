ae4271a59ac58810ca15574af8c4f9a7ca3cf030
mehmettilkicik <pbmemedo@gmail.com>
2023-11-26 21:25:32 +0300

    order

--- models/order.go
+++ models/order.go
@@ -3,9 +3,8 @@ package models
 import "time"
 
 type Order struct {
-	ID           uint `json:"primaryKey"`
-	CreatedAt    time.Time
-	TableRefer   int           `json:"table_id"`
-	Table        Table         `gorm:"foreignKey:TableRefer"`
-	OrderDetails []OrderDetail `gorm:"foreignKey:order_id"`
+	ID         uint `json:"id" gorm:"primaryKey"`
+	CreatedAt  time.Time
+	TableRefer int   `json:"table_id"`
+	Table      Table `gorm:"foreignKey:TableRefer"`
 }
--- routes/order.go
+++ routes/order.go
@@ -5,22 +5,12 @@ import (
 	"time"
 )
 
-type OrderDetail struct {
-	OrderID  uint `json:"order_id"`
-	Item     Item
-	Quantity uint `json:"quantity"`
-}
-
 type Order struct {
-	ID           uint `json:"id" gorm:"primaryKey"`
-	CreatedAt    time.Time
-	Table        Table
-	OrderDetails []OrderDetail `gorm:"foreignKey:order_id"`
+	ID        uint `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time
+	Table     Table
 }
 
-func CreateResponseOrder(order models.Order, table Table, orderdetails []OrderDetail) Order {
-	return Order{ID: order.ID, CreatedAt: order.CreatedAt, Table: table, OrderDetails: orderdetails}
-}
-func CreateResponseOrderDetail(orderdetail models.OrderDetail, order models.Order, Item Item) OrderDetail {
-	return OrderDetail{OrderID: order.ID, Item: Item, Quantity: orderdetail.Quantity}
+func CreateResponseOrder(order models.Order, table Table) Order {
+	return Order{ID: order.ID, CreatedAt: order.CreatedAt, Table: table}
 }
